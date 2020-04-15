166cf7171ed9c07ed5793d2eafa29f3c804f2d10
Rakesh Mothukuri <rockey5520@gmail.com>
2020-04-15 19:37:40 +0100

    optimized code as per the review comments

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -2,22 +2,26 @@ package hamming
 
 import (
 	"errors"
-	s "strings"
 )
 
 // Distance function calculates the hamming distance between two string
 func Distance(a, b string) (int, error) {
-	hammingdistance := 0
+
 	if len(a) != len(b) {
 		return 0, errors.New("strands are of different length")
 	}
-	if s.Compare(s.ToLower(a), s.ToLower(b)) == 0 {
-		return 0, nil
-	}
-	for i := 0; i < len(a); i++ {
-		if a[i] != b[i] {
+	runeA := []rune(a)
+	runeB := []rune(b)
+	hammingdistance := 0
+	for i := 0; i < len(runeA); i++ {
+		if runeA[i] != runeB[i] {
 			hammingdistance++
+		} else {
+			continue
 		}
+	}
+	if hammingdistance == 0 {
+		return 0, nil
 	}
 	return hammingdistance, nil
 }
