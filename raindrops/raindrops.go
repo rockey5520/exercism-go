37a03e3408d3ff4744013b0838bc6d4f226082c6
Rakesh Mothukuri <rockey5520@gmail.com>
2020-04-21 12:23:35 +0100

    refactored solution for raindrops

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -1,27 +1,24 @@
 package raindrops
 
 import (
-	"strings"
 	"strconv"
 )
+
 // Convert function converts integer to rain sounds
 func Convert(a int) string {
-	var sb strings.Builder
-	if ( a%3 == 0 && a%5 == 0 && a%7 == 0)  {
-		sb.WriteString("PlingPlangPlong")
-	}else if ( a%3 != 0 && a%5 != 0 && a%7 != 0)  {
-		v := int64(a)
-		sb.WriteString(strconv.FormatInt(v, 10))
-	}else{
-		if(a%3 == 0){
-			sb.WriteString("Pling")
-		}
-		if(a%5 == 0){
-			sb.WriteString("Plang")
-		}
-		if(a%7 == 0){
-			sb.WriteString("Plong")
-		}
+	var s string
+	if a%3 == 0 {
+		s += "Pling"
 	}
-	return sb.String()
+	if a%5 == 0 {
+		s += "Plang"
+	}
+	if a%7 == 0 {
+		s += "Plong"
+	}
+	if s == "" {
+		s += strconv.Itoa(a)
+		return s
+	}
+	return s
 }
