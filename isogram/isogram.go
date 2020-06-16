de75f6f2e02df8c9e81694880b4cf4c4fd3c9e53
Rakesh Mothukuri <rockey5520@gmail.com>
2020-06-16 10:29:27 +0100

    refactored isogram

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -2,17 +2,14 @@ package isogram
 
 import (
 	"strings"
-	s "strings"
 )
 
 // IsIsogram check given string if it is isogram or not
 func IsIsogram(input string) bool {
-	input = s.Replace(input, "-", "", -1)
-	input = s.Replace(input, " ", "", -1)
 	input = strings.ToLower(input)
-
 	for _, v := range input {
-		if strings.Count(input, string(v)) > 1 {
+
+		if strings.Count(input, string(v)) > 1 && v >= 'a' && v <= 'z' {
 			return false
 		}
 	}
